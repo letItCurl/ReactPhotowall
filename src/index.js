e1fad7bb3dc567199c8fb5a91dbc159c6994436a
rollpz <rolandlopez.developer@gmail.com>
2020-02-17 15:28:22 +0100

    Refractor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from  'react';
+import React, {Component} from  'react';
 import ReactDOM from 'react-dom'
 
-const tasks = ['take out the trash1','take out the trash2','take out the trash3']
+//const element = React.createElement('ol', null, tasks.map((task, index) => React.createElement('li', {key: index}, task)));
 
-const element = React.createElement('ol', null, tasks.map((task, index) => React.createElement('li', {key: index}, task)));
+class List extends Component {
+    render() { return(<ol>
+        {this.props.tasks.map((task, i) => <li key = {i}>{task}</li>)}
+        </ol>) }
+}
 
+class Title extends Component {
+    render() { return(<h1>{this.props.title}</h1>) }
 
-ReactDOM.render(element, document.getElementById('root'));
+}
+class Main extends Component {
+    render() {
+        return  <div>
+                    <Title title='Todo list'/>
+                    <List tasks={['First task', 'Second Task']}/>
+                    <List tasks={['First task2', 'Second Task2']}/>
+                </div>
+    }
+} 
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
