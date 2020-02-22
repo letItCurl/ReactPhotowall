230901a801db4e8a832b29b1d2164e892677e9e4
rollpz <rolandlopez.developer@gmail.com>
2020-02-22 16:43:32 +0100

    Going to firebase !

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -9,18 +9,19 @@ class Comments extends Component {
     handleSubmit(event){
         event.preventDefault()
         const comment = event.target.elements.comment.value
-        this.props.addComment(comment)
+        this.props.addComment(comment, this.props.id)
+        event.target.elements.comment.value = ''
     }
     render() {
+        console.log(this.props.comments)
         return (<div className="comment">
             {
                 this.props.comments.map((comment, index) => {
-                    
                     return (
                     <p key={index}> {comment} </p>
                     )
                 })
-            }
+                }
             <form className="comment-form" onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="comment" name="comment"/>
                 <input type="submit"/>
